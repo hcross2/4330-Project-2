3a3066368ea6d109fb9390186ab08fd20f37e724
Emmitt Bush <emmittbush@yahoo.com>
2015-11-07 12:21:44 -0600

    wrote define

--- Special/Define.cs
+++ Special/Define.cs
@@ -13,7 +13,21 @@ namespace Tree
             Printer.printDefine(t, n, p);
         }
         
-        public override void print()
+        public Node eval(Node t, Environment env)
+        {
+            Node identifier = t.getCdr().getCar();
+            Node value = t.getCdr().getCdr().getCar();
+            
+            if ((identifier.isSymbol())) //this is for if it is in the form of (define x 3), here x is not a function
+            {
+                env.define(identifier,value);
+            }
+            else
+            {
+                Closure idFunction = new Closure(new Cons(t.getCdr().getCar().getCdr(),t.getCdr().getCdr(), env));
+                env.define(identifier.getCar(), idFunction)
+            }
+        }
     }
 }
 
