2a92eb35de3e4004fc1fb780dc481d82e040abb4
Hunter Crossett <hunter.crossett@gmail.com>
2015-11-13 17:58:09 -0600

    butts
    biggies

--- Special/Regular.cs
+++ Special/Regular.cs
@@ -15,7 +15,45 @@ namespace Tree
         
         public override Node eval(Node t, Environment env)  //NEED TO DO THIS
         {
-            return null;
+            Node a = t.getCar();
+            Node b = evalRest(t.getCdr(), env);
+            while(a.isSymbol())
+            {
+                a = env.lookup(a);
+            }
+            if(a == null)
+            {
+                return null;
+            }
+            if(a.isProcedure())
+            {
+                return a.apply(b);
+            }
+            else
+            {
+                return a.eval(env).apply(b);
+            }
+        }
+        public Node evalRest(Node t, Environment env)
+        {
+            Node c;
+            if(t == null)
+            {
+                c = new Cons(new Nil(), new Nil());
+            }
+            else
+            {
+                Node car = t.getCar();
+                Node cdr = t.getCdr();
+                if(car.isSymbol())
+                {
+                    car = env.lookup(car);
+                }
+                
+                c = new Cons(car.eval(env), evalRest(cdr, env));
+                return c;
+            }
+            return c;
         }
     }
 }
